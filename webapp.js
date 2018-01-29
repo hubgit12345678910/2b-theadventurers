7a9a1a1f5948928e82b30d5fadac72550afbbb1f
Christopher Zhang <christopher.zhang@warriorlife.net>
2018-01-29 10:39:15 -0800

    Modified s3

--- webapp.js
+++ webapp.js
@@ -62,7 +62,7 @@ function updateScene(s) {
 			break;
 			case "s3":
 		
-			document.getElementById("scene_title").innerHTML = "The Beginning";
+			document.getElementById("scene_title").innerHTML = "";
 			document.getElementById("scene_text").innerHTML = "You wake up in the middle of a forest. You find a backpack containing a wooden sword, a linen tunic, and a flask of water. Will you continue deeper into the forest, or walk towards a nearby lake?";
 			
 			// change the first button
